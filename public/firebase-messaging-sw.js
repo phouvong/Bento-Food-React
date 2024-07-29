356a75cca318e34d953ffd2e27f88e40d3fa5684
phouvong <48210806+phouvong@users.noreply.github.com>
2024-07-29 19:10:29 +0700

    Update firebase-messaging-sw.js

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -5,13 +5,13 @@ importScripts(
     'https://www.gstatic.com/firebasejs/9.13.0/firebase-messaging-compat.js'
 )
 firebase?.initializeApp({
-    apiKey: '',
-    authDomain: '',
-    projectId: '',
-    storageBucket: '',
-    messagingSenderId: '',
-    appId: '',
-    measurementId: '',
+    apiKey: 'AIzaSyCHZCBLcVJf2NqZYsd37TzUxqzVcDI-2JU',
+    authDomain: 'bento-food.firebaseapp.com',
+    projectId: 'bento-food',
+    storageBucket: 'bento-food.appspot.com',
+    messagingSenderId: '299589180044',
+    appId: '1:299589180044:web:b575785f8db4c9d93f79a9',
+    measurementId: 'G-BS3XFY11Q6',
 })
 
 // Retrieve firebase messaging
