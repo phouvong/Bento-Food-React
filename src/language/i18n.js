02569c1186e033235b2444138fed665f9e000d2b
phouvong <pphouvong@gmail.com>
2024-11-19 23:01:23 +0700

    Update i18n.js

--- src/language/i18n.js
+++ src/language/i18n.js
@@ -1,7 +1,7 @@
 import i18n from 'i18next'
 import { initReactI18next } from 'react-i18next'
 import { english } from './en'
-import { english } from './lo'
+import { lao } from './lo'
 import { bengali } from './bn'
 import { arabic } from './ar'
 import { spanish } from './es'
