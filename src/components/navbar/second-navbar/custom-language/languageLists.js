3d7dee74167e07cbe965fb20b945b51e84b830ee
phouvong <48210806+phouvong@users.noreply.github.com>
2025-01-20 22:26:11 +0700

    Update languageLists.js

--- src/components/navbar/second-navbar/custom-language/languageLists.js
+++ src/components/navbar/second-navbar/custom-language/languageLists.js
@@ -4,11 +4,17 @@ import arFlag from '../../../../../public/static/country-flag/arabic-flag-svg.sv
 import banFlag from '../../../../../public/static/country-flag/bangladesh (1).png'
 import spanFlag from '../../../../../public/static/country-flag/spain.png'
 export const languageLists = [
+    //{
+    //    languageName: 'Arabic',
+    //    languageCode: 'ar',
+    //    countryCode: 'SA',
+    //    countryFlag: arFlag.src,
+    //},
     {
-        languageName: 'Arabic',
-        languageCode: 'ar',
-        countryCode: 'SA',
-        countryFlag: arFlag.src,
+        languageName: 'ພາສາລາວ',
+        languageCode: 'lo',
+        countryCode: 'LA',
+        countryFlag: loFlag.src,
     },
     {
         languageName: 'English',
@@ -16,18 +22,12 @@ export const languageLists = [
         countryCode: 'US',
         countryFlag: usFlag.src,
     },
-    {
-            languageName: 'ພາສາລາວ',
-            languageCode: 'lo',
-            countryCode: 'LA',
-            countryFlag: loFlag.src,
-        },
-    {
-        languageName: 'Spanish',
-        languageCode: 'es',
-        countryCode: 'es',
-        countryFlag: spanFlag.src,
-    },
+    //{
+    //    languageName: 'Spanish',
+    //    languageCode: 'es',
+    //    countryCode: 'es',
+    //    countryFlag: spanFlag.src,
+    //},
     //{
     //    languageName: 'Bengali',
     //    languageCode: 'bn',
