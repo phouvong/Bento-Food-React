6e7a7ab4b1374dbd987c7f9b703fd50a3ddbd190
phouvong <48210806+phouvong@users.noreply.github.com>
2025-05-14 16:07:32 +0700

    update 3.2

--- src/firebase.js
+++ src/firebase.js
@@ -6,15 +6,16 @@ import {
     isSupported,
 } from 'firebase/messaging'
 import { getAuth } from 'firebase/auth'
+import { useStoreFcm } from './hooks/react-query/push-notification/usePushNotification'
 
 const firebaseConfig = {
-    apiKey: '',
-    authDomain: '',
-    projectId: '',
-    storageBucket: '',
-    messagingSenderId: '',
-    appId: '',
-    measurementId: '',
+    apiKey: 'AIzaSyCHZCBLcVJf2NqZYsd37TzUxqzVcDI-2JU',
+    authDomain: 'bento-food.firebaseapp.com',
+    projectId: 'bento-food',
+    storageBucket: 'bento-food.firebasestorage.app',
+    messagingSenderId: '299589180044',
+    appId: '1:299589180044:web:b575785f8db4c9d93f79a9',
+    measurementId: 'G-BS3XFY11Q6',
 }
 const firebaseApp = !getApps().length ? initializeApp(firebaseConfig) : getApp()
 const messaging = (async () => {
@@ -30,19 +31,26 @@ const messaging = (async () => {
     }
 })()
 
-export const fetchToken = async (setFcmToken) => {
+export const fetchToken = async (setTokenFound, setFcmToken) => {
     return getToken(await messaging, {
         vapidKey: '',
     })
         .then((currentToken) => {
             if (currentToken) {
+                setTokenFound(true)
                 setFcmToken(currentToken)
+
+                // Track the token -> client mapping, by sending to backend server
+                // show on the UI that permission is secured
             } else {
+                setTokenFound(false)
                 setFcmToken()
+                // shows on the UI that permission is required
             }
         })
         .catch((err) => {
             console.error(err)
+            // catch error while creating client token
         })
 }
 
