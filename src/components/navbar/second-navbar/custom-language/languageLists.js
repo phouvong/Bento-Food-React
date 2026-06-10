83745aa2ebb1c83222f38d0b1c10ae45b204cd73
phouvong <48210806+phouvong@users.noreply.github.com>
2026-06-10 21:52:04 +0700

    Update languageLists.js

--- src/components/navbar/second-navbar/custom-language/languageLists.js
+++ src/components/navbar/second-navbar/custom-language/languageLists.js
@@ -1,5 +1,5 @@
-const loFlag = '/static/country-flag/lao.png'
-const englishFlag = '/static/country-flag/english.png'
+import loFlag from '../../../../../public/static/country-flag/lao.png'
+import usFlag from '../../../../../public/static/country-flag/english.png'
 import arFlag from '../../../../../public/static/country-flag/arabic-flag-svg.svg'
 import banFlag from '../../../../../public/static/country-flag/bangladesh (1).png'
 import spanFlag from '../../../../../public/static/country-flag/spain.png'
