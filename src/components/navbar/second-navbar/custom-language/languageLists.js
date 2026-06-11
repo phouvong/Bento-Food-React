b26c4c487099be977f452f6ca6154a8fdcf27c9e
Phouvong Phounsavath <48210806+phouvong@users.noreply.github.com>
2026-06-11 11:05:59 +0700

    Update languageLists.js

--- src/components/navbar/second-navbar/custom-language/languageLists.js
+++ src/components/navbar/second-navbar/custom-language/languageLists.js
@@ -1,5 +1,5 @@
-import loFlag from '../../../../../public/static/country-flag/lao.png'
-import usFlag from '../../../../../public/static/country-flag/english.png'
+import loFlag from '/public/static/country-flag/lao.png'
+import usFlag from '/public/static/country-flag/english.png'
 import arFlag from '../../../../../public/static/country-flag/arabic-flag-svg.svg'
 import banFlag from '../../../../../public/static/country-flag/bangladesh (1).png'
 import spanFlag from '../../../../../public/static/country-flag/spain.png'
