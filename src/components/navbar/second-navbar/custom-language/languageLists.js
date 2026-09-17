2562a94e8e4793b668d5b3c4a2a3b8b366ed00fc
phouvong <48210806+phouvong@users.noreply.github.com>
2026-09-17 18:21:28 +0700

    lao lang

--- src/components/navbar/second-navbar/custom-language/languageLists.js
+++ src/components/navbar/second-navbar/custom-language/languageLists.js
@@ -1,13 +1,20 @@
-import usFlag from '../../../../../public/static/country-flag/us.svg'
+import usFlag from '../../../../../public/static/country-flag/english.png'
+import loFlag from '../../../../../public/static/country-flag/lao.png'
 import arFlag from '../../../../../public/static/country-flag/arabic-flag-svg.svg'
 import banFlag from '../../../../../public/static/country-flag/bangladesh (1).png'
 import spanFlag from '../../../../../public/static/country-flag/spain.png'
 export const languageLists = [
+//    {
+//        languageName: 'Arabic (عربي)',
+//        languageCode: 'ar',
+//        countryCode: 'SA',
+//        countryFlag: arFlag.src,
+//    },
     {
-        languageName: 'Arabic (عربي)',
-        languageCode: 'ar',
-        countryCode: 'SA',
-        countryFlag: arFlag.src,
+        languageName: 'ພາສາລາວ (Lao)',
+        languageCode: 'lo',
+        countryCode: 'LA',
+        countryFlag: loFlag.src,
     },
     {
         languageName: 'English (English)',
@@ -15,16 +22,16 @@ export const languageLists = [
         countryCode: 'US',
         countryFlag: usFlag.src,
     },
-    {
-        languageName: 'Spanish (español)',
-        languageCode: 'es',
-        countryCode: 'es',
-        countryFlag: spanFlag.src,
-    },
-    {
-        languageName: 'Bangla (বাংলা)',
-        languageCode: 'bn',
-        countryCode: 'BN',
-        countryFlag: banFlag.src,
-    },
+//    {
+//        languageName: 'Spanish (español)',
+//        languageCode: 'es',
+//        countryCode: 'es',
+//        countryFlag: spanFlag.src,
+//    },
+//    {
+//        languageName: 'Bangla (বাংলা)',
+//        languageCode: 'bn',
+//        countryCode: 'BN',
+//        countryFlag: banFlag.src,
+//    },
 ]
